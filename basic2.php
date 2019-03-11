48fa2b0e076710bce90a1b18801a39f7c0e11419
Anesto27 <anes.2503@gmx.at>
2019-03-11 10:16:41 +0100

    finish basic2

---
+++ basic2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Basic2</title>
+</head>
+<body>
+	<?php 
+	$d=date("D");
+	if($d=="Monday")
+	echo "Happy Monday!";
+	elseif ($d!="Monday")
+	echo "Have a nice day!";
+	 ?>
+	
+</body>
+</html>
