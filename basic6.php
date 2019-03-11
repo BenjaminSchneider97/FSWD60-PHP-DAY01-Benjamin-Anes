8574a591bfe03472794550d64506dc06b7741117
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-03-11 14:55:09 +0100

    Add values to progress bar

--- basic6.php
+++ basic6.php
@@ -9,16 +9,14 @@
 
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<style>
-
-
 	.myrow{
 		display: flex;
 		justify-content: space-around;
 		position: absolute;
 		top: 50%;
 		transform: translateY(-50%);
+		text-align: center;
 	}
-
 	.thumbnail{
 		background-color: lightgray;
 		border-radius: 50px;
@@ -28,27 +26,30 @@
 		display: flex;
 		justify-content: space-around;
 	}
-
+	.value{
+		color: #000;
+		float: right;
+		margin-right: 10px;
+		font-size: 1.6em;
+		font-weight: bold;
+	}
 	.thumbnail img{
 		width: 500px;
 		height: 300px;
 	}
-
 	</style>
 </head>
 <body>
-	<h1>hero cards</h1>
 	<?php 
 		$heroes= array(
 			"Captain America"=>array(
-				"name"=> "<i class='fas fa-shield-alt'></i>Captain America",
+				"name"=> "<i class='fas fa-shield-alt'></i> Captain America",
 				"img"=> "img/america.jpg",
 				"height"=> 86,
 				"intelligence"=>40,
 				"strength"=>29,
 				"speed"=>40,
 				"fighting skills"=>86
-
 			),
 			"Iron Man"=>array(
 				"name"=> "<i class='fas fa-rocket'></i>Iron Man",
@@ -80,51 +81,51 @@
      					 <div class="caption">
        					 <h3>Stats</h3>
        					 <div class="row">
-       					  <p class="col-md-4">'."HEIGHT" . $value["height"]. '</p>
+       					  <p class="col-md-4">HEIGHT</p>
        					  <div class="col-md-8">
        						 <div class="progress">
  								 <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: '. $value["height"]. '%">
-    								 <span class="sr-only">80% Complete (success)</span>
+    								 <p class="value">'. $value["height"]. '</p>
   								 </div>
 							</div>
 							</div>
 							</div>
 							<div class="row">
-       					  <p class="col-md-4">'."INTELLIGENCE" . $value["intelligence"]. '</p>
+       					  <p class="col-md-4">INTELLIGENCE</p>
        					  <div class="col-md-8">
        						 <div class="progress">
  								 <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="4" aria-valuemin="0" aria-valuemax="100" style="width:'. $value["intelligence"]. '%">
-    								 <span class="sr-only">4% Complete (success)</span>
+    								 <p class="value">'. $value["intelligence"]. '</p>
   								 </div>
 							</div>
 							</div>
 							</div>
 							<div class="row">
-       					  <p class="col-md-4">'."STRENGTH" . $value["strength"]. '</p>
+       					  <p class="col-md-4">STRENGTH</p>
        					  <div class="col-md-8">
        						 <div class="progress">
  								 <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:'. $value["strength"]. '%">
-    								 <span class="sr-only">85% Complete (success)</span>
+    								 <p class="value">'. $value["strength"]. '</p>
   								 </div>
 							</div>
 							</div>
 							</div>
 							<div class="row">
-       					  <p class="col-md-4">'."SPEED" . $value["speed"]. '</p>
+       					  <p class="col-md-4">SPEED</p>
        					  <div class="col-md-8">
        						 <div class="progress">
  								 <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: '. $value["speed"]. '%">
-    								 <span class="sr-only">85% Complete (success)</span>
+    								 <p class="value">'. $value["speed"]. '</p>
   								 </div>
 							</div>
 							</div>
 							</div>
 						 <div class="row">
-       					  <p class="col-md-4">'."FIGHTINGSKILLS" . $value["fighting skills"]. '</p>
+       					  <p class="col-md-4">FIGHTINGSKILLS</p>
        					  <div class="col-md-8">
        						 <div class="progress">
  								 <div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: '. $value["fighting skills"]. '%">
-    								 <span class="sr-only">85% Complete (success)</span>
+    								 <p class="value">'. $value["fighting skills"]. '</p>
   								 </div>
 							</div>
 							</div>
