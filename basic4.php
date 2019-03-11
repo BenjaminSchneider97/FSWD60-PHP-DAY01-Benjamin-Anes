6a1d623a6b8ae1b77a744fb1b46bcd94fbcc78cf
Anesto27 <anes.2503@gmx.at>
2019-03-11 10:48:10 +0100

    finish basic4

--- basic4.php
+++ basic4.php
@@ -5,6 +5,29 @@
 	<title>basic4</title>
 </head>
 <body>
-	
+	<?php 
+	$a='Anes Smajic';
+
+		for ($i=1; $i < 51; $i++) { 
+			echo $i. $a. "<br>";
+			
+		}
+	 	$j=51;
+		while ( $j<= 100) {
+			echo $j. $a."<br>";
+			$j++;
+		}
+
+		$k=101;
+		do
+		{
+			echo $k. $a. "<br>";
+			$k++;
+			
+		}
+		while ( $k<= 150);
+	 ?>
+
+
 </body>
 </html>
