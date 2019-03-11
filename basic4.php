35ba4f918aa90de01a13066311e3ecc4d68abfa7
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-03-11 10:59:06 +0100

    prettified basic 4

--- basic4.php
+++ basic4.php
@@ -3,25 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<title>basic4</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
 </head>
 <body>
 	<?php 
 	$a='Anes Smajic';
+	$b='Benjo Schneider';
 
 		for ($i=1; $i < 51; $i++) { 
-			echo $i. $a. "<br>";
+			echo $i. " <i class='fas fa-arrow-right'></i> ". $a. " <i class='far fa-handshake'></i> ". $b. "<br>";
 			
 		}
 	 	$j=51;
 		while ( $j<= 100) {
-			echo $j. $a."<br>";
+			echo $j. " <i class='fas fa-arrow-right'></i> ". $a. " <i class='far fa-handshake'></i> ". $b. "<br>";
 			$j++;
 		}
 
 		$k=101;
 		do
 		{
-			echo $k. $a. "<br>";
+			echo $k. " <i class='fas fa-arrow-right'></i> ". $a. " <i class='far fa-handshake'></i> ". $b. "<br>";
 			$k++;
 			
 		}
